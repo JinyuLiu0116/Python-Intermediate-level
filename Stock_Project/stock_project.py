9e4750b39c941972bdde08478e9e47ed9b724dad
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-08-06 17:07:55 -0400

    Update stock_project.py

--- Stock_Project/stock_project.py
+++ Stock_Project/stock_project.py
@@ -13,18 +13,18 @@ from airflow.sensors.weekday import DayOfWeekSensor
 dburl='mysql+pymysql://root:8551649@localhost:3306/stock_data'
 engine = sqlalchemy.create_engine(dburl)
 
-ticker='WFC'
+ticker='wfc'
 
 class getData(beam.DoFn): # getData class inhernc from beam.DoFn
     def process(self,ticker): # check date for getting new data
         now=datetime.now()
-        dataQuery='SELECT MAX(Date) FROM wfc'
+        dataQuery=f'SELECT MAX(Date) FROM {ticker}'
         newData=pd.read_sql(dataQuery,engine).value[0][0]
         if newData is None:
             newData='2024-01-02'
         
-        stock_data=yf.download(ticker,start=newData,end=now)
-        for Date, row in stock_data.iterrows(): #format data to 3 decimal plays
+        df=yf.download(ticker,start=newData,end=now)
+        for Date, row in df.iterrows(): #format data to 3 decimal plays
             yield{
                 'symbol':ticker,
                 'Date':Date,

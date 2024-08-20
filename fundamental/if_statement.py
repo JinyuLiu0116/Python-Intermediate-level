b9c1bc14c566711b928fa9c1b36b0f7da4a73c08
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-08-20 19:42:20 -0400

    Add files via upload

---
+++ fundamental/if_statement.py
@@ -0,0 +1,35 @@
+age=int(input("Enter your age: "))
+
+if age>=18:
+    print("You are now signed up!")
+else:
+    print("You must be 18+ to sign up!")
+
+
+name=input("Enter your name:")
+if name == "":
+    print("You did not type in your name!")
+else:
+    print(f"Hello {name}.")
+
+print("Please enter your scoar for you subjects.")
+math=float(input("Math:"))
+englis=float(input("English:"))
+science=float(input("Science:"))
+cs=float(input("Computer Science:"))
+
+average=(math+englis+science+cs)/4
+
+if average>=90 & average<=100:
+    grade='A'
+elif average>=80 & average< 90:
+    grade='B'
+elif average>=70 & average< 80:
+    grade='C'
+elif average>=60 & average< 70:
+    grade='D'
+else:
+    grade='F'
+
+print(f"Your average: {average}")
+print(f"Your grade: {grade}")

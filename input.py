7de75b9908e9ca4dfa152b85f64d50a6df1239ea
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-08-19 20:21:59 -0400

    Add files via upload

---
+++ input.py
@@ -0,0 +1,19 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+age+=10
+
+print(f"Hello {name}")
+print(f"Your are {age} years old!")
+
+#mad libs
+adj1=input("Enter a adjective: ")
+noun=input("Enter a noun: ")
+adj2=input("Enter a adjective: ")
+verb=input("Enter a verb: ")
+adj3=input("Enter a adjective: ")
+
+print(f"Today I went to a {adj1} zoo.")
+print(f"In an exhibit, I saw {noun}")
+print(f"{noun} was {adj2} and {verb}ing")
+print(f"I was {adj3}")
+

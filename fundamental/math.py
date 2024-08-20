eef4575d4164a6a26e5ffb0cf16ed4e9d9225f96
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-08-19 23:22:58 -0400

    Add files via upload

---
+++ fundamental/math.py
@@ -0,0 +1,29 @@
+x =3.14
+y = -5
+z = 6
+
+power=z**2
+power2=pow(z,2)
+print(power)
+print(power2)
+
+result=round(x)
+print(result)
+
+result2=abs(y)
+print(result2)
+
+Max=max(x, y, z)
+Min=min(x, y, z)
+
+print(f"Max:{Max}, Min:{Min}")
+
+import math
+print(f"PI: {math.pi}")
+print(f"e: {math.e}")
+print(f"squrt root of 9 is: {math.sqrt(9)}")
+print(f"The ceilling of {x} is: {math.ceil(x)}")
+print(f"The floor of {x} is: {math.floor(x)}")
+
+
+

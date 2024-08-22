025e47d9620b941e325b9b1069c87d06c4361530
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-08-22 13:02:15 -0400

    Add files via upload

---
+++ fundamental/if_statement#4.py
@@ -0,0 +1,26 @@
+num =-5
+print("Number is postivate" if num >0 else "Number is negative")
+
+num1=6
+result="Even" if num1 % 2==0 else "Odd"
+print(f"The {num1} is {result}")
+
+a = float(print("Enter number a: ")) 
+b = float(print("Enter number b: "))
+max_num = a if a > b else b
+print(f"The max number between {a} and {b} is {max_num}")
+min_num = a if a < b else b
+print(f"The min number between {a} and {b} is {min_num}")
+
+age=int(print("Enter your age: "))
+status = "Adult" if age >= 18 else "Child"
+print(f"Your are {age} years old, you are an/a {status}")
+
+temperature = float(print("Enter today's temperature:"))
+weather="Hot" if temperature >=35 else "Cold"
+print(f"Today's {temperature} degree, it is {weather}")
+
+user_role = "Admin"
+access_level= "Full access" if user_role == "Admin" or user_role == "admin" else "limited access"
+print(f"You have {access_level}")
+

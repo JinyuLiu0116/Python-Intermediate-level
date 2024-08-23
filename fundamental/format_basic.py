fcbd8eba4b4f810cc0301a2db174464b78bb9d01
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-08-23 14:45:21 -0400

    Add files via upload

---
+++ fundamental/format_basic.py
@@ -0,0 +1,24 @@
+price1= 345.12354
+price2= -945.6851
+price3= 45.21468
+
+print(f"Price 1: ${price1:.2f}")
+print(f"Price 2: ${price2:.3f}")
+print(f"Price 3: ${price3:.1f}")
+
+price1= 34500.12354
+price2= -94005.6851
+price3= 450000.21468
+
+print(f"Price 1: ${price1:20}")
+print(f"Price 2: ${price2:20}")
+print(f"Price 3: ${price3:20}")
+
+price1= 34500.12354
+price2= -94005.6851
+price3= 450000.21468
+
+print(f"Price 1: ${price1:+,.2f}")
+print(f"Price 2: ${price2:+,.2f}")
+print(f"Price 3: ${price3:+,.2f}")
+

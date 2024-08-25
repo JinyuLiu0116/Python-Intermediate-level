29d156752c00976d433673051a97d454b5f909ec
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-08-24 21:11:05 -0400

    Update MySQL_basic#2.py

--- MySQL_database/MySQL_basic#2.py
+++ MySQL_database/MySQL_basic#2.py
@@ -20,10 +20,25 @@ cursor.execute("""
     Length_minutes INT);
 """)
 
+cursor.execute("""
+    INSERT INTO movie(Id, Title, Director, Year, Length-minutes) VALUES
+    (1, 'Toy Story', 'John Lasseter', 1995, 81),
+    (2, "A Bug's Life", 'John Lasseter', 1998, 95),
+    (3,	'Toy Story 2', 'John Lasseter',	1999, 93),
+    (4,	'Monsters, Inc.', 'Pete Docter', 2001, 92),
+    (5,	'Finding Nemo',	'Andrew Stanton', 2003, 107),
+    (6,	'The Incredibles', 'Brad Bird',	2004, 116),
+    (7,	'Cars',	'John Lasseter', 2006, 117),
+    (8,	'Ratatouille', 'Brad Bird',	2007, 115),
+    (9,	'WALL-E', 'Andrew Stanton',	2008, 104),
+    (10, 'Up', 'Pete Docter', 2009, 101),
+    (11, 'Toy Story 3',	'Lee Unkrich', 2010, 103),
+    (12, 'Cars 2', J'ohn Lasseter',	2011, 120),
+    (13, 'Brave', 'Brenda Chapman', 2012, 102),
+    (14, 'Monsters University', 'Dan Scanlon', 2013, 110);
+""")
+
 cursor.execute("DESCRIBE movie")
 
 for i in cursor:
     print(i,end=" ")
-
-cursor.execute()
-

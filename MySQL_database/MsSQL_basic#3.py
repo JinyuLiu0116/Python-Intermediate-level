443032bd82bf56d28ec507233958e053a87a6c50
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-08-27 21:52:56 -0400

    Add files via upload

---
+++ MySQL_database/MsSQL_basic#3.py
@@ -0,0 +1,42 @@
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "8551649",
+    database = "practice"
+)
+
+cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM movie")
+result_table=cursor.fetchall()
+for row in result_table:
+    print(row)
+
+cursor.execute("SELECT title FROM movie")
+result_title=cursor.fetchall()
+print("The title of movies:")
+for row in result_title:
+    print(row)
+
+cursor.execute("SELECT director FROM movie")
+result_director=cursor.fetchall()
+print("The director of movies:")
+for row in result_director:
+    print(row)
+
+cursor.execute("SELECT title,director FROM movie")
+result_T_D=cursor.fetchall()
+print("The title and director of movies:")
+for row in result_T_D:
+    print(row)
+
+cursor.execute("SELECT titile,year FROM movie")
+result_T_Y=cursor.fetchall()
+print("The title and year of movies:")
+for row in result_T_Y:
+    print(row)
+
+cursor.close()
+conn.close()

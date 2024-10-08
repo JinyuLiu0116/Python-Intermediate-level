3fc441681e97074d565db693bcbce462376f1093
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-10-07 21:10:15 -0400

    Update classes#5.py

--- OOP/classes#5.py
+++ OOP/classes#5.py
@@ -11,3 +11,8 @@ emp_1.firstName = 'Corey'
 emp_1.lastName = 'Schater'
 emp_1.email = 'Corey.Schater@company.com'
 emp_1.pay = 50000
+
+emp_2.firstName = 'Test'
+emp_2.lastName = 'User'
+emp_2.email = 'Test.User@company.com'
+emp_2.pay = 5000

66333bce39c5d9beadf0fe3aff9773014a0ff5a9
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-10-07 21:20:51 -0400

    Update classes#5.py

--- OOP/classes#5.py
+++ OOP/classes#5.py
@@ -8,18 +8,20 @@ class Employee:
 emp_1 = Employee('Corey', 'Schater', 50000)
 emp_2 = Employee('Test', 'User', 5000)
 
-print(emp_1)
-print(emp_2)
+#print(emp_1)
+#print(emp_2)
 
-emp_1.firstName = 'Corey'
-emp_1.lastName = 'Schater'
-emp_1.email = 'Corey.Schater@company.com'
-emp_1.pay = 50000
+#emp_1.firstName = 'Corey'
+#emp_1.lastName = 'Schater'
+#emp_1.email = 'Corey.Schater@company.com'
+#emp_1.pay = 50000
 
-emp_2.firstName = 'Test'
-emp_2.lastName = 'User'
-emp_2.email = 'Test.User@company.com'
-emp_2.pay = 5000
+#emp_2.firstName = 'Test'
+#emp_2.lastName = 'User'
+#emp_2.email = 'Test.User@company.com'
+#emp_2.pay = 5000
 
 print(emp_1.email)
 print(emp_2.email)
+
+print('{} {}'.format(emp_1.fistName, emp_1.lastName)

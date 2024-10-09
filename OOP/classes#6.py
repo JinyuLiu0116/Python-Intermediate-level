4cb5a7803208c141dd2faeb612e75d5e8f721f23
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-10-08 20:17:14 -0400

    Update classes#6.py

--- OOP/classes#6.py
+++ OOP/classes#6.py
@@ -1,1 +1,12 @@
+class Employee:
+  def __init__(self, firstName, lastName, pay):
+    self.firstName = firstName
+    self.lastName = lastName
+    self.pay = pay
+    self.email = fristName + '.' + lastName + '@company.com'
 
+  def getFullNmae(self):
+    return self.firstName + ' ' + self.lastNmae
+
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Test', 'User', 5000)

c3ff9c6167f8ad2a4d54dee53e43648a856a80e4
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-10-08 20:27:18 -0400

    Update classes#6.py

--- OOP/classes#6.py
+++ OOP/classes#6.py
@@ -1,4 +1,5 @@
 class Employee:
+  raise_amount = 1.04
   def __init__(self, firstName, lastName, pay):
     self.firstName = firstName
     self.lastName = lastName
@@ -8,5 +9,12 @@ class Employee:
   def getFullNmae(self):
     return '{} {}'.format(self.firstName, self.lastName)
 
+  def apply_raise(self):
+    self.pay = int(self.pay * Employee.raise_amount)
+
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('Test', 'User', 5000)
+
+emp_1.apply_raise()
+print(emp_1.pay)
+print(emp_1.__dict__) #check all elements

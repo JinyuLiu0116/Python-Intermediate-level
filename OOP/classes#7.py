88c01a6d392f774b6d392bf7687985118fdb3a3c
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-10-18 22:59:53 -0400

    Update classes#7.py

--- OOP/classes#7.py
+++ OOP/classes#7.py
@@ -1,1 +1,13 @@
+class Employee:
+    num_of_emps = 0
+    raise_amount = 1.04
 
+    def __init__(self, firstName, lastName, pay):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = firstName + ' ' + lastName + '@email.com'
+        self.pay = pay
+        Employee.num_of_emps += 1
+
+    def getFullName(self):
+        return '{} {}'.format(self.firstName, self.lastName)

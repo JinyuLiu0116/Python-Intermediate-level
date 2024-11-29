3e4dcde8154bb479a4d61518e55578a725305236
Jinyu Liu <ojin0116@gmail.com>
2024-11-28 23:30:37 -0500

    update

--- Data structure/Linked-list.py
+++ Data structure/Linked-list.py
@@ -1,10 +1,10 @@
-
+#Node class is responsible for storing
 class Node:
 
     def __init__(self, value):
         self.value = value
         self.next = None
-
+#a container class
 class LinkedList:
 
     def __init__(self):
@@ -20,7 +20,13 @@ class LinkedList:
         pass
 
     def append(self, value):
-        pass
+        if self.head is None:
+            self.head = Node(value)
+        else:
+            last = self.head
+            while last.next:
+                last = last.next
+            last.next = Node(value)
 
     def prepend(self, value):
         pass

73f01eb5d8be52bee7eb9dbd28635301f25f0b55
Jinyu Liu <ojin0116@gmail.com>
2024-11-28 23:08:09 -0500

    update

--- Data structure/Linked-list.py
+++ Data structure/Linked-list.py
@@ -5,3 +5,41 @@ class Node:
         self.value = value
         self.next = None
 
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def __repr__(self):
+        pass
+
+    def __contains__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def append(self, value):
+        pass
+
+    def prepend(self, value):
+        pass
+
+    def insrt(self, value, index):
+        pass
+
+    def delete(self, value):
+        pass
+
+    def pop(self, index):
+        pass
+
+    def get(self, index):
+        pass
+
+    def print(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass

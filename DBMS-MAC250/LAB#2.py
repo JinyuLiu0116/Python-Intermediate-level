718d96cb67bef3111f0d20d4c3c40b19b93796bb
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-01-30 22:50:52 -0500

    Update LAB#2.py

--- DBMS-MAC250/LAB#2.py
+++ DBMS-MAC250/LAB#2.py
@@ -1,1 +1,27 @@
+import mysql.connector
 
+def query_mysql_executor(query):
+    conn = None
+    cursor = None
+    result = None
+    conn = mysql.connector.connect(
+        host ='localhost',
+        user = 'root',
+        password = '8551649',
+        database = 'premiere'
+    )
+    if conn:
+        print('Connection has been built')
+    
+    cursor = conn.cursor()
+    if cursor:
+        print('Cursor has been created')
+
+    query = query
+    cursor.execute(query)
+    result = cursor.fetchall()
+    if cursor:
+        cursor.close()
+    if conn:
+        conn.close()
+    return result

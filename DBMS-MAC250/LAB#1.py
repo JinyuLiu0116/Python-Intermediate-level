c93078af1193657e9a2853c0580b558336c6af14
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-01-31 23:18:17 -0500

    Update LAB#1.py

--- DBMS-MAC250/LAB#1.py
+++ DBMS-MAC250/LAB#1.py
@@ -1,19 +1,31 @@
 import mysql.connector
-conn = None
-cursor = None
-try:
-    conn = mysql.connector.connect(
-        host = 'localhost',
-        user = 'root',
-        password = '8551649',
-        database = 'premiere'
-    )
-    if conn:
-        print('Connection has been built')
 
-    cursor = conn.cursor()
-    if cursor:
-        print('Cursor has been built')
+def query_mysql_executor(query):
+    conn = None
+    cursor = None
+    try:
+        conn = mysql.connector.connect(
+            host = 'localhost',
+            user = 'root',
+            password = '8551649',
+            database = 'premiere'
+        )
+        if conn:
+            print('Connection has been built')
+
+        cursor = conn.cursor()
+        if cursor:
+            print('Cursor has been built')
+    except Exception as e:
+        raise Exception(f"Job Failed {e}")
+    
+    finally:
+        if cursor:
+            cursor.close()
+            print('Cursor has been closed')
+        if conn:
+            conn.close()
+            print('Connection has been closed')
 
     #List the part number, description, and price for part.
     query = 'SELECT partNum, description, price FROM part'
@@ -158,14 +170,3 @@ try:
     print('#15')
     for row in cursor.fetchall():
         print(row)
-
-except Exception as e:
-    raise Exception(f"Job Failed {e}")
-
-finally:
-    if cursor:
-        cursor.close()
-        print('Cursor has been closed')
-    if conn:
-        conn.close()
-        print('Connection has been closed')

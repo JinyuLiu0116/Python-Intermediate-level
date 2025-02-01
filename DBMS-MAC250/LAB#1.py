1371962cdece687e702bf27a9a583392ea48cc07
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-01-31 23:29:59 -0500

    Update LAB#1.py

--- DBMS-MAC250/LAB#1.py
+++ DBMS-MAC250/LAB#1.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-def query_mysql_executor(query):
+def query_mysql_executor(query, num):
     conn = None
     cursor = None
     try:
@@ -16,6 +16,9 @@ def query_mysql_executor(query):
         cursor = conn.cursor()
         if cursor:
             print('Cursor has been built')
+        cursor.execute(query)
+        result = cursor.fetchall()
+        print(f"#{num}: {result}\n")
     except Exception as e:
         raise Exception(f"Job Failed {e}")
     
@@ -28,145 +31,94 @@ def query_mysql_executor(query):
             print('Connection has been closed')
 
     #List the part number, description, and price for part.
-    query = 'SELECT partNum, description, price FROM part'
-    cursor.execute(query)
-    
-    result = cursor.fetchall()
-    print('#1')
-    for row in result:
-        print(row)
+query = 'SELECT partNum, description, price FROM part'
+num = 1
+query_mysql_executor(query=query,num=num)
+
 
     #2.List all rows and columns for the complete orders table
-    query = 'SELECT * FROM orders;'
-    cursor.execute(query)
-    result = cursor.fetchall()
-    print('#2')
-    for row in result:
-        print(row)
-        
-    #3.List the names of customers with credit limits of $10,000 or more.
-    query = 'SELECT customerName FROM customer WHERE creditLimit >= 10000;'
-    cursor.execute(query)
-    result = cursor.fetchall()
-    print('#3')
-    for row in result:
-        print(row)
-        
-    #4.List the order number for each placed by customer number 608 on 10/23/2010
-    query = """SELECT orderNum FROM orders 
-            WHERE customerNum = 608 AND orderDate = '2010-10-23';"""
-    cursor.execute(query)
-    result = cursor.fetchall()
-    print('#4')
-    for row in result:
-        print(row)
-
-    #5.List the number and name of each customer represented by sales rep 35 or sales rep 65
-    query = """
-                SELECT customerNum, customerName FROM customer
-                WHERE repNum = '35' OR repNum = '65';
-            """
-    cursor.execute(query)
-    result = cursor.fetchall()
-    print('#5')
-    for row in result:
-        print(row)
+query = 'SELECT * FROM orders;'
+num = 2
+query_mysql_executor(query=query,num=num)
     
-    #6.List the part number and part description of each part that is not in item class AP
-    query = """SELECT partNum, description FROM part WHERE class != 'AP';"""
-    cursor.execute(query)
-    result = cursor.fetchall()
-    print('#6')
-    for row in result:
-        print(row)
-
-    #7.List the part number, description, and number of units on hand for each part that
-    #  has between 10 and 25 units on hand, including both 10 and 25. Do this in two ways
-    query1 = """SELECT partNum, description, onHand FROM part WHERE onHand BETWEEN 10 AND 25;"""
-    query2 = """
-                Select partNum, description, onHand FROM part
-                WHERE onHand >= 10 AND onHand <= 25;
-             """
-    print('#7')
-    cursor.execute(query1)
-    result1 = cursor.fetchall()
-    for row in result1:
-        print(row)
-
-    cursor.execute(query2)
-    result2 = cursor.fetchall()
-    for row in result2:
-        print(row)
+#3.List the names of customers with credit limits of $10,000 or more.
+query = 'SELECT customerName FROM customer WHERE creditLimit >= 10000;'
+num = 3
+query_mysql_executor(query=query,num=num)
     
-    #8.List the part number, part description, and on hand value of each part in item class SG
-    #  Assign the name ON_HAND_VALUE to the computed column.
-    query = """SELECT partNum, description, onHand * price AS ON_HAND_VALUE
-               FROM part WHERE class = 'SG';"""
-    cursor.execute(query)
-    result = cursor.fetchall()
-    print('#8')
-    for row in result:
-        print(row)
-    
-    #9.List the part number, part description, and onhand value for each part whose on hand
-    #  value is ate least$7,500. 
-    query = """SELECT partNum, description, onHand * price AS ON_HAND_VALUE
-               FROM part WHERE onHand * price >= 7500;"""
-    cursor.execute(query)
-    result = cursor.fetchall()
-    print('#9')
-    for row in result:
-        print(row)
+#4.List the order number for each placed by customer number 608 on 10/23/2010
+query = """SELECT orderNum FROM orders 
+        WHERE customerNum = 608 AND orderDate = '2010-10-23';"""
+num = 4
+query_mysql_executor(query=query,num=num)
+#5.List the number and name of each customer represented by sales rep 35 or sales rep 65
+query = """
+            SELECT customerNum, customerName FROM customer
+            WHERE repNum = '35' OR repNum = '65';
+        """
+num = 5
+query_mysql_executor(query=query,num=num)
 
-    #10.Use the IN operator to list the part number and part description of each part in item class
-    #   AP or SG
-    query = """SELECT partNum, description FROM part
-               WHERE class IN('AP', 'SG');"""
-    cursor.execute(query)
-    result = cursor.fetchall()
-    print('#10')
-    for row in result:
-        print(row)
-    
-    #11.Find the number and name of each customer whose name begins with the letter"B"
-    query = """SELECT customerNum, customerName FROM customer
-               WHERE customerName LIKE 'B%';"""
-    cursor.execute(query)
-    result = cursor.fetchall()
-    print('#10')
-    for row in result:
-        print(row)
+#6.List the part number and part description of each part that is not in item class AP
+query = """SELECT partNum, description FROM part WHERE class != 'AP';"""
+num = 6
+query_mysql_executor(query=query,num=num)
+#7.List the part number, description, and number of units on hand for each part that
+#  has between 10 and 25 units on hand, including both 10 and 25. Do this in two ways
+query1 = """SELECT partNum, description, onHand FROM part WHERE onHand BETWEEN 10 AND 25;"""
+query2 = """
+            Select partNum, description, onHand FROM part
+            WHERE onHand >= 10 AND onHand <= 25;
+         """
+num = 7.1
+query_mysql_executor(query=query1,num=num)
+num = 7.2
+query_mysql_executor(query=query2,num=num)
 
-    #12.List all details about all parts. Order the output by part description
-    query = """SELECT * FROM part ORDER BY description;"""
-    cursor.execute(query)
-    result = cursor.fetchall()
-    print('#12')
-    for row in result:
-        print(row)
+#8.List the part number, part description, and on hand value of each part in item class SG
+#  Assign the name ON_HAND_VALUE to the computed column.
+query = """SELECT partNum, description, onHand * price AS ON_HAND_VALUE
+           FROM part WHERE class = 'SG';"""
+num = 8
+query_mysql_executor(query=query,num=num)
 
-    #13.List all details about all parts. order the output by part number
-    #   within warehouse(That is, order the output by warehouse and the by part number)
-    query = """SELECT * FROM part ORDER BY warehouse, partNum;"""
-    cursor.execute(query)
-    print('#13')
-    for row in cursor.fetchall():
-        print(row)
-    
-    #14.How many customers have balances that are more than their credit limits?
-    query = """SELECT COUNT(*) FROM customer
-               WHERE balance > creditLimit;"""
-    cursor.execute(query)
-    print('#14')
-    for row in cursor.fetchall():
-        print(row)
+#9.List the part number, part description, and onhand value for each part whose on hand
+#  value is ate least$7,500. 
+query = """SELECT partNum, description, onHand * price AS ON_HAND_VALUE
+           FROM part WHERE onHand * price >= 7500;"""
+num = 9
+query_mysql_executor(query=query,num=num)
+#10.Use the IN operator to list the part number and part description of each part in item class
+#   AP or SG
+query = """SELECT partNum, description FROM part
+           WHERE class IN('AP', 'SG');"""
+num = 10
+query_mysql_executor(query=query,num=num)
 
-    #15.Find the total of the balances for all customers represented by sales
-    #   rep 65 with balances that are less than their credit limits
-    query = """SELECT SUM(balance) FROM customer
-               WHERE repNum = '65'
-               AND balance < creditLimit;"""
-    cursor.execute(query)
-    print('#15')
-    for row in cursor.fetchall():
-        print(row)
+#11.Find the number and name of each customer whose name begins with the letter"B"
+query = """SELECT customerNum, customerName FROM customer
+           WHERE customerName LIKE 'B%';"""
+num = 11
+query_mysql_executor(query=query,num=num)
+#12.List all details about all parts. Order the output by part description
+query = """SELECT * FROM part ORDER BY description;"""
+num = 12
+query_mysql_executor(query=query,num=num)
+#13.List all details about all parts. order the output by part number
+#   within warehouse(That is, order the output by warehouse and the by part number)
+query = """SELECT * FROM part ORDER BY warehouse, partNum;"""
+num = 13
+query_mysql_executor(query=query,num=num)
+
+#14.How many customers have balances that are more than their credit limits?
+query = """SELECT COUNT(*) FROM customer
+           WHERE balance > creditLimit;"""
+num = 14
+query_mysql_executor(query=query,num=num)
+#15.Find the total of the balances for all customers represented by sales
+#   rep 65 with balances that are less than their credit limits
+query = """SELECT SUM(balance) FROM customer
+           WHERE repNum = '65'
+           AND balance < creditLimit;"""
+num = 15
+query_mysql_executor(query=query,num=num)

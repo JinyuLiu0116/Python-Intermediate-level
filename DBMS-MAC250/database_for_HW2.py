3c18396e4e9115c8af5cb1723961acfbe3badc1b
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-02-02 16:16:49 -0500

    Update database_for_HW2.py

--- DBMS-MAC250/database_for_HW2.py
+++ DBMS-MAC250/database_for_HW2.py
@@ -1,1 +1,23 @@
+conn = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = '8551649',
+    database = 'company'
+)
 
+# query = """CREATE TABLE employee(
+#             f_name CHAR(30),
+#             m_name CHAR(1),
+#             l_name CHAR(30),
+#             e_ssn CHAR(9),
+#             date DATE,
+#             address CHAR(50),
+#             salary INT,
+#             sup_e_ssn CHAR(9),
+#             d_num CHAR(1)
+#             );"""
+query = """ALTER TABLE employee
+            ADD CONSTRAINT pk_employee
+            PRIMARY KEY(e_ssn);"""
+cursor = conn.cursor()
+cursor.execute(query)

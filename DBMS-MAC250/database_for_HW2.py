a947a1cec849eb8afc63d521888f0013cc836e2d
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-02-02 17:47:46 -0500

    Update database_for_HW2.py

--- DBMS-MAC250/database_for_HW2.py
+++ DBMS-MAC250/database_for_HW2.py
@@ -4,35 +4,25 @@ conn = mysql.connector.connect(
     password = '8551649',
     database = 'company'
 )
-
-query = """CREATE TABLE employee(
-            f_name CHAR(30),
-            m_name CHAR(1),
-            l_name CHAR(30),
-            e_ssn CHAR(9),
-            date DATE,
-            address CHAR(50),
-            sex,
-            salary INT,
-            sup_e_ssn CHAR(9),
-            d_num CHAR(1)
-            );"""
-
-query = """ALTER TABLE employee
-            ADD CONSTRAINT pk_employee
-            PRIMARY KEY(e_ssn);"""
-
-
 query = """CREATE TABLE department(
             d_name CHAR(30),
             d_num CHAR(1) PRIMARY KEY,
             mgr_ssn CHAR(9),
             mgr_start_date DATE);"""
 
-query = """ALTER TABLE employee
-            ADD CONSTRAINT fk_employee
-            FOREIGN KEY(d_num)
-            REFERENCES department(d_num);"""
+query = """CREATE TABLE employee(
+            f_name CHAR(30),
+            m_name CHAR(1),
+            l_name CHAR(30),
+            e_ssn CHAR(9) PIRMARY KEY,
+            date DATE,
+            address CHAR(50),
+            sex CHAR(1),
+            salary DECIMAL(6,2),
+            sup_e_ssn CHAR(9),
+            d_num CHAR(1),
+            FOREIGN KEY(d_num) REFERENCES department(d_num)
+            );"""
 
 query ="""
         CREATE TABLE dept_locations(
@@ -59,6 +49,13 @@ query = """
         p_num CHAR(2),
         hours DECIMAL(4,2),
         PRIMARY KEY(e_ssn, p_num));"""
+
+query = """
+        ALTER TABLE works_on
+        ADD CONSTRAINT fk_works_e
+        FOREIGN KEY(e_ssn) REFERENCES employee(e_ssn),
+        ADD CONSTRAINT fk_works_project
+        FOREIGN KEY(p_num) REFERENCES project(p_num);"""
 
 cursor = conn.cursor()
 cursor.execute(query)

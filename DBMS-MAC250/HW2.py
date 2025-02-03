ae9ded2971a6fa178a6b3ef074689d43a86b3045
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-02-03 12:15:52 -0500

    Update HW2.py

--- DBMS-MAC250/HW2.py
+++ DBMS-MAC250/HW2.py
@@ -1,11 +1,23 @@
 import mysql.connector
 
-conn = mysql.connector.connect(
-    host = 'localhost',
-    user = 'root',
-    password = '8551649',
-    database = 'company'
-)
+def query_mysql_executor(query):
+    conn = mysql.connector.connect(
+        host = 'localhost',
+        user = 'root',
+        password = '8551649',
+        database = 'company'
+    )
+    try:
+        cursor = conn.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+    except Exception as e:
+        raise Exception('Query failed %s', e)
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
 
 #a) Retrieve the names of all employees in Department 5 who work more than 10 hours per week on the ‘Product X’ project. 
 query = """
@@ -17,12 +29,9 @@ query = """
         AND d.d_num = '5'
         AND hours > 10
         AND p.p_name = 'productx';"""
-cursor = conn.cursor()
-cursor.execute(query)
-result = cursor.fetchall()
+result = query_mysql_executor(query)
+print(f"#1: {result}")
 
-for row in result:
-    print(row)
 # b) List the names of all employees who have a dependent with the same first name as themselves. 
 query2 = """
         SELECT f_name, m_name, l_name
@@ -30,12 +39,8 @@ query2 = """
         WHERE e.e_ssn = d.e_ssn
         AND e.f_name = d.dependent_name;
         """
-cursor = conn.cursor()
-cursor.execute(query2)
-result2 = cursor.fetchall()
-
-for row in result2:
-    print(row)
+result2 = query_mysql_executor(query2)
+print(f"#2: {result2}")
 # c) Find the names of all employees who are directly supervised by ‘Franklin Wong’. 
 query3 = """
         SELECT f_name, m_name, l_name
@@ -45,12 +50,8 @@ query3 = """
                             WHERE f_name = 'Franklin'
                             AND l_name = 'Wong');
         """
-cursor = conn.cursor()
-cursor.execute(query3)
-result3 = cursor.fetchall()
-
-for row in result3:
-    print(row)
+result3 = query_mysql_executor(query3)
+print(f"#3: {result3}")
 # d) For each project, list the project name and the total hours per week (by all employees) spent on that project. 
 query4 = """
         SELECT p_name, SUM(hours) as total_hours
@@ -58,45 +59,28 @@ query4 = """
         WHERE p.p_num = w.p_num
         GROUP BY p_name;"""
 
-cursor = conn.cursor()
-cursor.execute(query4)
-result4 = cursor.fetchall()
-
-for row in result4:
-    print(row)
+result4 = query_mysql_executor(query4)
+print(f"#4: {result4}")
 # e) For each department, retrieve the department name and the average salary of all employees working in that department.
 query5 = """SELECT d_name, AVG(salary) as avg_salary
             FROM department d, employee e
             WHERE d.d_num = e.d_num
             GROUP BY d_name;""" 
-cursor = conn.cursor()
-cursor.execute(query5)
-result5 = cursor.fetchall()
-
-for row in result5:
-    print(row)
+result5 = query_mysql_executor(query5)
+print(f"#5: {result5}")   
 # f) Retrieve the name of employees who work on EVERY project. 
 query6 = """SELECT f_name, m_name, l_name AS name
             FROM employee e
             WHERE e.e_ssn =(SELECT e_ssn FROM works_on GROUP BY e_ssn HAVING COUNT(p_num) = (
 		        SELECT COUNT(p_num) FROM project));"""
 
-cursor = conn.cursor()
-cursor.execute(query6)
-result6 = cursor.fetchall()
-
-for row in result6:
-    print(row)
+result6 = query_mysql_executor(query6)
+print(f"#6: {result6}")
 #g) Retrieve the average salary of all female employees. 
 query7 ="""SELECT AVG(salary) FROM employee
             WHERE sex = 'M';"""
-cursor = conn.cursor()
-print('#7')
-cursor.execute(query7)
-result7 = cursor.fetchall()
-
-for row in result7:
-    print(row)
+result7 = query_mysql_executor(query7)
+print(f"#7: {result7}")
 # h) Find the names and addresses of all eemployees who work on at least one project located in Houston, 
 # but whose department has no location in Houston. 
 query8 = """SELECT f_name, m_name, l_name, address
@@ -108,13 +92,8 @@ query8 = """SELECT f_name, m_name, l_name, address
             AND p.p_location = 'HOUSTON'
             AND d.d_location != 'HOUSTON'
             GROUP BY f_name, m_name, l_name, address;"""
-cursor = conn.cursor()
-print('#8')
-cursor.execute(query8)
-result8 = cursor.fetchall()
-
-for row in result8:
-    print(row)
+result8 = query_mysql_executor(query8)
+print(f"#8: {result8}")
 # i) List the last names of all department managers who have no dependents.
 query9 = """SELECT l_name
             FROM employee
@@ -122,15 +101,5 @@ query9 = """SELECT l_name
             (SELECT mgr_ssn FROM department
              WHERE mgr_ssn NOT IN
                 (SELECT e_ssn FROM dependent));"""
-cursor = conn.cursor()
-print('#9')
-cursor.execute(query9)
-result9 = cursor.fetchall()
-
-for row in result9:
-    print(row)
-
-if cursor:
-    cursor.close()
-if conn:
-    conn.close()
+result9 = query_mysql_executor(query9)
+print(f"#9: {result9}")

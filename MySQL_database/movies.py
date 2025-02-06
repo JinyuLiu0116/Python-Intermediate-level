acf6a7be391aee3a1be61765c2497b3ae5874eba
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-02-05 21:42:38 -0500

    Update movies.py

--- MySQL_database/movies.py
+++ MySQL_database/movies.py
@@ -1,1 +1,16 @@
+import mysql.connector
 
+conn = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = '8551649',
+    database = 'movies'
+)
+
+def query_mysql_executor(query, conn):
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(query)
+        return cursor.fetchall()
+    except Exception as e:
+        raise Exception(f"query failed {e}")

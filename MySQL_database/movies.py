b0fcfd1edecf5b26991f7ba1122980a5be1b37a3
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-02-05 21:45:41 -0500

    Update movies.py

--- MySQL_database/movies.py
+++ MySQL_database/movies.py
@@ -8,9 +8,15 @@ conn = mysql.connector.connect(
 )
 
 def query_mysql_executor(query, conn):
+    result = None
     try:
         with conn.cursor() as cursor:
             cursor.execute(query)
-        return cursor.fetchall()
+            result = cursor.fetchall()
+        return result
     except Exception as e:
         raise Exception(f"query failed {e}")
+
+#Find all the Toy Story movies      
+query1 = """SELECT * FROM movies WHERE title LIKE '%Toy Story%';"""
+print(f"#1. {query_mysql_executor(query1, conn=conn)}")

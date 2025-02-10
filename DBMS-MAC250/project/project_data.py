4e6f8a8d19e2d3bdeea4d5e763dd4327ed899eb7
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-02-10 12:57:03 -0500

    Update project_data.py

--- DBMS-MAC250/project/project_data.py
+++ DBMS-MAC250/project/project_data.py
@@ -11,7 +11,16 @@ def query_mysql_executor(query, conn):
     try:
         with conn.cursor() as cursor:
             cursor.execute(query)
+        conn.commit()   
     except Exception as e:
-        raise Exception(f"query is failed at:{e}")
+        print(f"query is failed at:{e}")
     
+query = """INSERT INTO ins VALUES
+            ('GHI', 'Group Health Insurance', '11-11 11th st', 'Longisland City','NY','11101'),
+            ('BC', 'Blue Cross', '22-22 22th st', 'Sunnyside','NY','11102'),
+            ('AHS', 'American Health System', '33-33 33th st', 'Woodside','NY','11103');"""
+query_mysql_executor(query=query,conn=conn)
 
+
+if conn:
+    conn.close()

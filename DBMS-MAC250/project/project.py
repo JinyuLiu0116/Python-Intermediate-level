856de698bfa7f50b45710eefc732069220572ab1
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-02-09 21:31:42 -0500

    Update project.py

--- DBMS-MAC250/project/project.py
+++ DBMS-MAC250/project/project.py
@@ -3,10 +3,19 @@ import mysql.connector
 conn = mysql.connector.connect(
     host = 'localhost',
     user = 'root',
-    password = '8551649'
+    password = '8551649',
+    database = 'doctor'
 )
 
 cursor = conn.cursor()
 
-query ="""CREATE DATABASE doctor;"""
+# query ="""CREATE DATABASE doctor;"""
+# cursor.execute(query)
+query = """CREATE TABLE ins(
+            ins_code CHAR(5) PRIMARY KEY,
+            ins_name CHAR(30),
+            address CHAR(30),
+            city CHAR(30),
+            state CHAR(2),
+            zipcode CHAR(5));"""
 cursor.execute(query)

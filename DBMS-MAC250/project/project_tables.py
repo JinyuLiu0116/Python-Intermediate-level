b49dbafe2a189c8cbc89e3776ce71a80e4c250b8
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-02-09 21:49:23 -0500

    Update project_tables.py

--- DBMS-MAC250/project/project_tables.py
+++ DBMS-MAC250/project/project_tables.py
@@ -7,46 +7,55 @@ conn = mysql.connector.connect(
     database = 'doctor'
 )
 
-cursor = conn.cursor()
+def query_mysql_executor(query, conn):
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(query)
+            print(cursor.fetchall())
+    except Exception as e:
+        print("Query is failed.")
 
-# query ="""CREATE DATABASE doctor;"""
-# cursor.execute(query)
-# query = """CREATE TABLE ins(
-#             ins_code CHAR(5) PRIMARY KEY,
-#             ins_name CHAR(30),
-#             address CHAR(30),
-#             city CHAR(30),
-#             state CHAR(2),
-#             zipcode CHAR(5));"""
+#query ="""CREATE DATABASE doctor;"""
 
-# query = """CREATE TABLE patients(
-#             p_code CHAR(5) PRIMARY KEY,
-#             p_name CHAR(30),
-#             ins_code CHAR(5),
-#             address_line1 CHAR(30),
-#             address_line2 CHAR(30),
-#             city CHAR(30),
-#             state CHAR(2),
-#             zipcode CHAR(5),
-#             FOREIGN KEY(ins_code) REFERENCES ins(ins_code));"""
-
-# query = """CREATE TABLE mds(
-#             mds_code CHAR(5) PRIMARY KEY,
-#             mds_name CHAR(30),
-#             address CHAR(30),
-#             city CHAR(30),
-#             state CHAR(2),
-#             zipcode CHAR(5));"""
-
-# query = """CREATE TABLE medical(
-#             mp_code CHAR(5) PRIMARY KEY,
-#             description CHAR(30),
-#             price DECIMAL(6,2));"""
-
-query = """CREATE TABLE treatments(
+query1 = """CREATE TABLE ins(
+            ins_code CHAR(5) PRIMARY KEY,
+            ins_name CHAR(30),
+            address CHAR(30),
+            city CHAR(30),
+            state CHAR(2),
+            zipcode CHAR(5));"""
+print(f"#1 {query_mysql_executor(query=query1,conn=conn)}")
+query2 = """CREATE TABLE patients(
+            p_code CHAR(5) PRIMARY KEY,
+            p_name CHAR(30),
+            ins_code CHAR(5),
+            address_line1 CHAR(30),
+            address_line2 CHAR(30),
+            city CHAR(30),
+            state CHAR(2),
+            zipcode CHAR(5),
+            FOREIGN KEY(ins_code) REFERENCES ins(ins_code));"""
+print(f"#2 {query_mysql_executor(query=query2,conn=conn)}")
+query3 = """CREATE TABLE mds(
+            mds_code CHAR(5) PRIMARY KEY,
+            mds_name CHAR(30),
+            address CHAR(30),
+            city CHAR(30),
+            state CHAR(2),
+            zipcode CHAR(5));"""
+print(f"#3 {query_mysql_executor(query=query3,conn=conn)}")
+query4 = """CREATE TABLE medical(
+            mp_code CHAR(5) PRIMARY KEY,
+            description CHAR(30),
+            price DECIMAL(6,2));"""
+print(f"#4 {query_mysql_executor(query=query4,conn=conn)}")
+query5 = """CREATE TABLE treatments(
             p_code CHAR(5),
             mds_code CHAR(5),
             mp_code CHAR(5),
             date DATE,
             PRIMARY KEY(p_code, mds_code, mp_code));"""
-cursor.execute(query)
+print(f"#5 {query_mysql_executor(query=query5,conn=conn)}")
+
+if conn:
+    conn.close()

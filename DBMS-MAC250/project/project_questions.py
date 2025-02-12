df2e601ed481b2b785097be89860b87674f16f54
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-02-11 21:07:34 -0500

    Update project_questions.py

--- DBMS-MAC250/project/project_questions.py
+++ DBMS-MAC250/project/project_questions.py
@@ -24,3 +24,10 @@ print(f"#1: {query_mysql_executor(query=query,conn=conn)}")
 #Print the cpt (Mp_code) record that has the highest price
 query2 = """SELECT * FROM medical WHERE price = (SELECT MAX(price) FROM medical);"""
 print(f"#2: {query_mysql_executor(query=query2,conn=conn)}")
+
+#Print the name of the patient that has the most treatments (# of records in the treatments file) 
+query3 = """SELECT p_name FROM patients WHERE p_code IN 
+                (SELECT p_code FROM treatments GROUP BY p_code HAVING COUNT(p_code) = 
+                    (SELECT MAX(t_count) FROM (SELECT COUNT(p_code) AS t_count 
+                        FROM treatments GROUP BY p_code)AS subquery));"""
+print(f"#3: {query_mysql_executor(query=query3,conn=conn)}")

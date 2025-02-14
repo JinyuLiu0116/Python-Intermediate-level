42e1239fa0f860cf6e83771b224cbe9a9110d185
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-02-13 20:28:28 -0500

    Update LAB#3.py

--- DBMS-MAC250/LAB#3.py
+++ DBMS-MAC250/LAB#3.py
@@ -16,4 +16,10 @@ def query_mysql_executor(query, conn):
         return result
     except Exception as e:
         print(f"Query failed at: {e}")
-
+query1 = """CREATE TABLE nonappliance(
+            partNum CHAR(4) PRIMARY KEY,
+            description CHAR(15),
+            onHand DECIMAL(4,0),
+            class CHAR(2),
+            price DECIMAL(6,2));"""
+print(f"#1: {query_mysql_executor(query=query1, conn=conn)}")

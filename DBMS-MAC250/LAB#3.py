9c62e7013d7c25560d8fc32e450a4a5f1a005ecf
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-02-13 20:27:56 -0500

    Update LAB#3.py

--- DBMS-MAC250/LAB#3.py
+++ DBMS-MAC250/LAB#3.py
@@ -1,1 +1,19 @@
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = '8551649',
+    database = 'premiere'
+)
+
+def query_mysql_executor(query, conn):
+    result = None
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(query)
+            result = cursor.fetchall()
+        return result
+    except Exception as e:
+        print(f"Query failed at: {e}")
 

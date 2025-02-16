93a138d438c7f432df22df481b7597fb7c012780
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-02-16 11:46:06 -0500

    Update HW3.py

--- DBMS-MAC250/HW3.py
+++ DBMS-MAC250/HW3.py
@@ -1,1 +1,11 @@
+import mysql.connector
 
+conn = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = '8551649',
+    database = 'henry_books'
+)
+
+if conn:
+    print('connection has been created')

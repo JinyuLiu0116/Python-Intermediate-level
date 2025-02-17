8e7d7736d0d513c64b700b8a61bba4db2008c3f9
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-02-17 17:32:03 -0500

    Update LAB#4.py

--- DBMS-MAC250/LAB#4.py
+++ DBMS-MAC250/LAB#4.py
@@ -21,7 +21,10 @@ def query_mysql_executor(query, conn):
 # 1 Create a view named major_customer. It consists of the customer number, name, balance, credit limit, 
 # and rep number for every customer whose credit limit is $10000 or less.
 # a.	Write and execute the CREATE VIEW command to create the major_customer view.
-query1_a = """"""
+query1_a = """CREATE VIEW major_customer 
+                AS SELECT customerNum, customerName, balance, creditLimit, repNum
+                FROM customer
+                WHERE creditLimit <= 10000;"""
 query_mysql_executor(query= query1_a, conn=conn)
 
 

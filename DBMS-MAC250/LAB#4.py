e1402c47f6f8b660f3b47d83d9ea22303fc27234
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-02-17 16:55:41 -0500

    Update LAB#4.py

--- DBMS-MAC250/LAB#4.py
+++ DBMS-MAC250/LAB#4.py
@@ -1,4 +1,3 @@
-
 import mysql.connector
 
 conn = mysql.connector.connect(
@@ -8,8 +7,22 @@ conn = mysql.connector.connect(
     database = 'premiere'
 )
 
+def query_mysql_executor(query, conn):
+    result = None
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(query)
+            result = cursor.fetchall()
+        for row in result:
+            print(row)
+    except Exception as e:
+        print(f"query failed:{e}")
 
-
+# 1 Create a view named major_customer. It consists of the customer number, name, balance, credit limit, 
+# and rep number for every customer whose credit limit is $10000 or less.
+# a.	Write and execute the CREATE VIEW command to create the major_customer view.
+query1_a = """"""
+query_mysql_executor(query= query1_a, conn=conn)
 
 
 

408fc8dd0b89fed410215a7259158db42f01ee9d
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-02-17 16:52:04 -0500

    Update LAB#4.py

--- DBMS-MAC250/LAB#4.py
+++ DBMS-MAC250/LAB#4.py
@@ -1,1 +1,17 @@
 
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = '8551649',
+    database = 'premiere'
+)
+
+
+
+
+
+
+if conn:
+    conn.close()

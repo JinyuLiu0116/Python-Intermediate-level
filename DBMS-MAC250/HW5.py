6291a8277ae906ee28acb5c5342cd242d61bbd88
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-02-19 14:54:11 -0500

    Update HW5.py

--- DBMS-MAC250/HW5.py
+++ DBMS-MAC250/HW5.py
@@ -7,3 +7,21 @@ conn = mysql.connector.connect(
     password = '8551649',
     database = 'company'
 )
+
+data = [
+    {"id":101, "name":'Jinyu Liu', "age":55},
+    {"id":102, "name":'Qinyu Qiu', "age":45},
+    {"id":103, "name":'Tinyu Tiu', "age":35},
+    {"id":104, "name":'Binyu Biu', "age":25}
+]
+
+with open('data.csv', mode= 'w', newline='') as file:
+    fieldnames = ["id", "name", "age"]
+    writer= csv.DictWriter(file, fieldnames=fieldnames)
+
+    writer.writeheader()
+    writer.writerows(data)
+
+
+if conn:
+    conn.close()

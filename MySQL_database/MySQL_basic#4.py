12eb42780a9a586d0c129bbb03116a7bc5945c91
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-02-21 23:09:14 -0500

    Update MySQL_basic#4.py

--- MySQL_database/MySQL_basic#4.py
+++ MySQL_database/MySQL_basic#4.py
@@ -18,23 +18,19 @@ def query_mysql_executor(query, conn):
 
 cursor.execute("SELECT id, title FROM movie WHERE id = 6")
 result1 = cursor.fetchall()
-for i in result1:
-    print(i)
+
 
 cursor.execute("SELECT year, title FROMR movie WHERE year BETWEEN 2000 AND 2010")
 result2 = cusor.fetchall()
-for i in result2:
-    print(i)
+
     
 cursor.execute("SELECT year, title FROM movie WHERE year < 2000 or year > 2010")
 result3 = cursor.fetchall()
-for i in result3:
-    print(i)
+
 
 cursor.execute("SELECT title, year FROM movie WHERE year <= 2003")
 result4 = cursor.fetchall()
-for i in result4:
-    print(i)
 
-cursor.close()
-conn.close()
+
+if conn:
+    conn.close()

2b7be751f01895b15f5f732d701abcea0ad0d6a3
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-02-24 11:33:58 -0500

    Update data_for_HW4.py

--- DBMS-MAC250/data_for_HW4.py
+++ DBMS-MAC250/data_for_HW4.py
@@ -1,1 +1,15 @@
-
+data1 = """INSERT INTO AUTHOR VALUES ('185', 'Benson', 'Reeves', '1990');
+            INSERT INTO AUTHOR VALUES ('218', 'Rachel', 'Beatney', '1983');
+            INSERT INTO AUTHOR VALUES ('229', 'Carmine', 'Salvadore', NULL);
+            INSERT INTO AUTHOR VALUES ('251', 'Hugo', 'Bruer', '1972');
+            INSERT INTO AUTHOR VALUES ('262', 'Xia', 'Chiang', NULL);
+            INSERT INTO AUTHOR VALUES ('273', 'Reba', 'Durante', '1969');
+            INSERT INTO AUTHOR VALUES ('284', 'Trina', 'Tankersly', '1961');
+            INSERT INTO AUTHOR VALUES ('383', 'Neal', 'Walsh', '1980');
+            INSERT INTO AUTHOR VALUES ('394', 'Robert', 'Lake', '1982');
+            INSERT INTO AUTHOR VALUES ('438', 'Perry', 'Pearson', '1986');
+            INSERT INTO AUTHOR VALUES ('460', 'Connie', 'Paulsen', '1983');
+            INSERT INTO AUTHOR VALUES ('559', 'Rachel', 'McGill', NULL);
+            INSERT INTO AUTHOR VALUES ('581', 'Manish', 'Aggerwal', '1984');
+            INSERT INTO AUTHOR VALUES ('592', 'Lawrence', 'Sheel', '1976');
+            INSERT INTO AUTHOR VALUES ('603', 'Julia', 'Palca', '1988');"""

453d96157a1644fbb1e7a9d24489863984ef22f6
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-02-24 11:31:21 -0500

    Update tables_for_HW4.py

--- DBMS-MAC250/tables_for_HW4.py
+++ DBMS-MAC250/tables_for_HW4.py
@@ -6,3 +6,18 @@ conn = mysql.connector.connect(
     password = '8551649',
     database = 'library'
 )
+
+def query_mysql_executor(query, conn):
+    result = None
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(query)
+            result = cursor.fetchall()
+        for row in result:
+            print(row)
+        print('-----------------------------------------------')
+    except Exception as e:
+        print(f"query failed: {e}")
+        
+if conn:
+    conn.close()

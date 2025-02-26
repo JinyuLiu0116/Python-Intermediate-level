e49c6d74cbd885ea1041478f2dff9815426c34be
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-02-25 20:17:19 -0500

    Update HW4.py

--- DBMS-MAC250/HW4.py
+++ DBMS-MAC250/HW4.py
@@ -21,3 +21,7 @@ def query_mysql_execute(query, conn):
 # 1. Write a query to display the different subjects on which FACT has books. Include each subject only once 
 query1 = """SELECT book_subject FROM book GROUP BY book_subject;"""
 query_mysql_execute(query=query1,conn=conn)
+
+# 2. Write a query to display the checkout number, book number, and checkout date of all books checked out before April 5, 2015
+query2 = """SELECT checkNum, bookNum, check_out_date FROM checkout WHERE check_out_date < '2015-04-05';"""
+query_mysql_execute(query=query2,conn=conn)
